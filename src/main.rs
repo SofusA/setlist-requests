eaac8016dc856fa4438407da80c5cd7f94f49ea2
Sofus Addington <sofus@addington.dk>
2024-08-28 21:07:31 +0200

    add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use setlist_requests::{html, view::View};
+use tower_http::trace;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .compact()
+        .init();
+
+    let router = axum::Router::new()
+        .route("/", axum::routing::get(keys))
+        .layer(
+            tower_http::trace::TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(tracing::Level::DEBUG))
+                .on_response(trace::DefaultOnResponse::new().level(tracing::Level::DEBUG)),
+        );
+
+    let address = "0.0.0.0:3000";
+    tracing::info!("listening on {}", address);
+
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+async fn keys() -> View {
+    html! {
+        <div class="flex justify-center w-full">
+                hello
+            </div>
+    }
 }
---
+++ src/view.rs
@@ -0,0 +1,51 @@
+#[macro_export]
+macro_rules! html {
+    ($($body:tt)*) => {
+        View::new(rstml_to_string_macro::html!($($body)*))
+    };
+}
+
+#[derive(Clone)]
+pub struct View(String);
+
+impl View {
+    pub fn new(string: String) -> View {
+        View(string)
+    }
+}
+
+impl Default for View {
+    fn default() -> Self {
+        Self::new(String::new())
+    }
+}
+
+impl axum::response::IntoResponse for View {
+    fn into_response(self) -> axum::response::Response {
+        (
+            [(
+                axum::http::header::CONTENT_TYPE,
+                axum::http::HeaderValue::from_static(mime::TEXT_HTML_UTF_8.as_ref()),
+            )],
+            self.0,
+        )
+            .into_response()
+    }
+}
+
+impl std::fmt::Display for View {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromIterator<View> for View {
+    fn from_iter<T: IntoIterator<Item = View>>(iter: T) -> Self {
+        let mut result = String::new();
+
+        for view in iter {
+            result += &view.0;
+        }
+        View(result)
+    }
+}
